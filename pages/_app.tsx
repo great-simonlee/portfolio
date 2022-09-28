e131a95fc18b676748ba4edbbe51f28aabcb2de0
leejh <leejh@helixtech.co.kr>
2022-09-28 11:46:09 +0900

    setting mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default App;
